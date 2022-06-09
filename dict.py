056894fbbec620f04f7b519fbe55c9b145bf3cfd
thrivenienugu <enuguthriveni@gmail.com>
2022-06-09 14:30:05 +0200

    Added Main function for while loop

--- dict.py
+++ dict.py
@@ -40,20 +40,24 @@ def read_print():
     LIST   - list all phone numbers
     QUIT   - quit the program''')
 
+def main():
+    while True: ## REPL - Read Execute Program Loop
+        cmd = input("Command: ")
+        if cmd == "list":
+            print(read_dict(conn))
+        elif cmd == "add":
+            name = input("  Word: ")
+            phone = input("  Translation: ")
+            add_word(conn, name, phone)
+            print(f" Added word {name}")
+        elif cmd == "delete":
+            ID = input("  ID: ")
+            delete_word(conn, ID)
+            print(f" Deleted {ID}")
+        elif cmd == "quit":
+            save_dict(conn)
+            exit()
+
+
 read_print()
-while True: ## REPL - Read Execute Program Loop
-    cmd = input("Command: ")
-    if cmd == "list":
-        print(read_dict(conn))
-    elif cmd == "add":
-        name = input("  Word: ")
-        phone = input("  Translation: ")
-        add_word(conn, name, phone)
-        print(f" Added word {name}")
-    elif cmd == "delete":
-        ID = input("  ID: ")
-        delete_word(conn, ID)
-        print(f" Deleted {ID}")
-    elif cmd == "quit":
-        save_dict(conn)
-        exit()
+main()
